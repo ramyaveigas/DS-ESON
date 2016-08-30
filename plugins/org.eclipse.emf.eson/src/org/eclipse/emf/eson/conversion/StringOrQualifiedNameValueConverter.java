91d05993c9b6cdcba8316514ccf8e7d3fb9f678a
yandenmatten <yandenmatten@D505BC72.europe.temenosgroup.com>
2016-08-30 22:01:10 +0200

    DS-10662 - CLONE - ESON- Application with Field name containing dot in the end or at the begining will cause error in Data eson
    wrong headers

--- plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/conversion/StringOrQualifiedNameValueConverter.java
+++ plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/conversion/StringOrQualifiedNameValueConverter.java
@@ -1,18 +1,14 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2014 Michael Vorburger and others.
- * All rights reserved.   This program and the accompanying materials
+/*
+ * #%L
+ * org.eclipse.emf.eson
+ * %%
+ * Copyright (C) 2016 Michael Vorburger and others. 
+ * %%
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
- *   Yann Andenmatten - Initial API and implementation
- *
- * </copyright>
- *
- * 
+ * #L%
  */
 package org.eclipse.emf.eson.conversion;
 
