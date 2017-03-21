9efcaf5740e0c9c088373b833162559a5e681e64
atripod <atripod@D4SZ9WC2.europe.temenosgroup.com>
2017-03-21 13:40:10 +0100

    DS-11537 fix header

--- plugins/org.eclipse.emf.eson.ui/src/org/eclipse/emf/eson/ui/editor/model/ESONJavaClassPathResourceForIEditorInputFactory.java
+++ plugins/org.eclipse.emf.eson.ui/src/org/eclipse/emf/eson/ui/editor/model/ESONJavaClassPathResourceForIEditorInputFactory.java
@@ -1,3 +1,15 @@
+/*
+ * #%L
+ * org.eclipse.emf.eson
+ * %%
+ * Copyright (C) 2017 Michael Vorburger and others.
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
 package org.eclipse.emf.eson.ui.editor.model;
 
 import org.eclipse.core.resources.IStorage;
@@ -6,7 +18,9 @@ import org.eclipse.emf.eson.validation.ESONResourceValidationUtils;
 import org.eclipse.xtext.ui.editor.model.JavaClassPathResourceForIEditorInputFactory;
 
 /**
- * DS-11537
+ * DS-11537 - Check if the validation must be enabled when the resource is
+ * opened in the editor.
+ * 
  * @author atripod
  */
 public class ESONJavaClassPathResourceForIEditorInputFactory extends JavaClassPathResourceForIEditorInputFactory {
